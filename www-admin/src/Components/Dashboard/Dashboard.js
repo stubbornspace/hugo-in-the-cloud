c0c88ccf2eb40b9936847db2d66883119f0a17b0
demo <demo@demo.local>
2020-02-15 16:11:41 -0800

    table

--- www-admin/src/Components/Dashboard/Dashboard.js
+++ www-admin/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 class Dashboard extends React.Component {
  
@@ -6,14 +7,30 @@ class Dashboard extends React.Component {
         
         return (
             <div>
-                THIS WILL BE A LIST OF POSTS
-
-                title
-                tags
-                description
-                date
-                status
-                select 
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                        <th>Ttitle</th>
+                        <th>Create Data</th>
+                        <th>Status</th>
+                        <th>Tags</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Fish Curry</td>
+                        <td>12/24/2020</td>
+                        <td>Published</td>
+                        <td>Fish, Curry, Thai</td>
+                        </tr>
+                        <tr>
+                        <td>Fish Curry</td>
+                        <td>12/24/2020</td>
+                        <td>Published</td>
+                        <td>Fish, Curry, Thai</td>
+                        </tr>
+                    </tbody>
+                </Table>
 
             </div>
         )
